e19183125af4ee29eae942992d385aff7f46f687
SimonXVB <167611089+SimonXVB@users.noreply.github.com>
2025-04-30 20:18:29 +0200

    added github link + changed upload screen

--- src/Components/MainPage.tsx
+++ src/Components/MainPage.tsx
@@ -24,8 +24,15 @@ export function MainPage() {
     return (
         <>
             {!pdfCTX.pdfDoc && 
-                <div className="h-screen flex justify-center items-center">
+                <div className="relative h-screen flex flex-col justify-center items-center gap-20">
+                    <div className="px-2">
+                        <p className="text-5xl font-bold italic text-cyan-500">PDEdit</p>
+                        <p className="font-semibold">Edit PDFs easily with PDEdit, a simple and open-source PDF Editor.</p>
+                    </div>
                     <UploadButton/>
+                    <a className="absolute bottom-0 left-0 m-5 block bg-gray-800 rounded-2xl p-1.5 hover:bg-gray-800/80 hover:-translate-y-1" href="https://github.com/SimonXVB/PDEdit" target="_blank">
+                        <img src="/github-mark-white.png" alt="GitHub Logo" width={50}/>
+                    </a>
                 </div>
             }
             {pdfCTX.pdfDoc &&
