709f8f20b3e242fd9a819b40e22492f6eb20e7b1
SimonXVB <167611089+SimonXVB@users.noreply.github.com>
2025-08-01 18:57:38 +0200

    updated root.tsx import

--- src/Components/Pages/HomePage.tsx
+++ src/Components/Pages/HomePage.tsx
@@ -1,9 +1,10 @@
 import { UploadButton } from "./Individuals/UploadButton.tsx";
+import logo from "../../Assets/Logo.png";
 
 export function HomePage() {
     return (
         <div className="h-screen flex flex-col justify-center items-center gap-25">
-            <img src="src/Assets/Logo.png" className="px-4" width={500} alt="PD-Edit Logo"/>
+            <img src={logo} className="px-4" width={500} alt="PD-Edit Logo"/>
             <UploadButton/>
         </div>
     )
--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Main } from './Main';
+import { Main } from './Main.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
