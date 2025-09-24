464839c63f1897a1a38968f3c5c0369cf3bd3f80
SimonXVB <167611089+SimonXVB@users.noreply.github.com>
2025-09-24 15:03:35 +0200

    fixed export bug

--- src/Context/PDFCTX/PDFContextProvider.tsx
+++ src/Context/PDFCTX/PDFContextProvider.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { PDFDocument } from "pdf-lib";
-import { pdfContext, PDFPagesInterface } from "./pdfContext";
+import { pdfContext } from "./pdfContext";
+import { PDFPageProxy } from "pdfjs-dist";
 
 export function PDFContextProvider({ children }: { children: React.ReactNode }) {
     const [pdfDoc, setPDFDoc] = useState<PDFDocument>();
-    const [pdfPages, setPDFPages] = useState<PDFPagesInterface[]>([]);
+    const [pdfPages, setPDFPages] = useState<PDFPageProxy[]>([]);
 
     return (
         <pdfContext.Provider value={{ pdfDoc, setPDFDoc, pdfPages, setPDFPages }}>
